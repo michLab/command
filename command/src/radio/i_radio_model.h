337f5982cccb121fcde4e419ef60e10e177981b6
Michal Labowski <michal.labowski@gmail.com>
2022-08-03 17:43:34 +0200

    feat: Add i_radio_model

---
+++ command/src/radio/i_radio_model.h
@@ -0,0 +1,36 @@
+#ifndef RADIO_MODEL_H_
+#define RADIO_MODEL_H_
+
+#include <cstdint>
+
+namespace radio {
+    using Power = uint32_t;
+    using Freq = uint32_t;
+    enum class State {active, passive};
+
+    class IRadioModel {
+        public:
+            virtual ~IRadioModel() = 0;
+            virtual void set_power(Power p) = 0;
+            virtual void set_freq(Freq f) = 0;
+            virtual void set_state(State s) = 0;
+            virtual Power get_power() = 0;
+            virtual Freq get_freq() = 0;
+            virtual State get_state() = 0;
+    };
+}
+
+#endif // RADIO_MODEL_H_
+
+
+/*
+Zadania do wykonania:
+- ustaw:
+    - moc
+    - częstotliość
+    - aktywność
+- odczytaj:
+    - moc
+    - częstotliwość
+    - aktywność
+*/
